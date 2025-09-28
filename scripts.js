37e5dffb038135a56bd5b25e1220d0c83e3d4512
AisaQc <aisa.qc@gmail.com>
2025-09-28 18:44:40 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ const products = [
   {
     name: 'Combat Noise',
     type: 'Vinilo',
-    price: 1,
+    price: 100,
     description: 'Frontline Offensive Force, 12" Picture',
     includesShipping: true,
     images: [
@@ -18,7 +18,7 @@ const products = [
   {
     name: 'Heretique',
     type: 'Vinilo',
-    price: 1,
+    price: 100,
     description: 'Bestias Hominum, 12" Incluye bonus track',
     options: ['Edición Clasica', 'Edición Picture'],
     includesShipping: false,
